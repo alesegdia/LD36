afe64dcc15878151cb1a7aa1df08b9d19466f1ce
Alejandro Seguí <alesegdia@gmail.com>
2016-08-28 08:13:43 +0200

    adding command class

---
+++ src/core/scene/command.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include <alligator/util/math.h>
+
+class Command
+{
+public:
+	typedef std::shared_ptr<Command> SharedPtr;
+
+	enum class Status : uint8_t
+	{
+		Running,
+		Ready
+	};
+
+	virtual ~Command();
+
+	Status status()
+	{
+		return m_status;
+	}
+
+	Status operator()(const Vec2i& tile)
+	{
+		delegate(tile);
+		return m_status;
+	}
+
+	virtual void delegate(const Vec2i& tile) = 0 ;
+
+protected:
+	void status( Status new_status )
+	{
+		m_status = new_status;
+	}
+
+private:
+	Status m_status;
+
+};
